125197d59d1583992c7bad727c7ec148d74fe701
Тимофей <t.irishin@inbox.ru>
2026-03-17 18:23:36 +0300

    Создана библиотека классов Models - Добавлена модель LogEntry для хранения записи лога - Добавлена модель IPStatistics для статистики по IP адресам - Добавлена модель PageStatistics для статистики по страницам - Выделение моделей в отдельную библиотеку для соблюдения MVC

---
+++ Models/LogEntry.cs
@@ -0,0 +1,12 @@
+﻿namespace Models
+{
+    public class LogEntry
+    {
+        public string IP { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string Method { get; set; }
+        public string Url { get; set; }
+        public int StatusCode { get; set; }
+        public int Size { get; set; }
+    }
+}
---
+++ Models/PageStatistics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class PageStatistics
+    {
+        public string Url { get; set; }
+        public int RequestCount { get; set; }
+        public double Percentage { get; set; }
+        public DateTime FirstSeen { get; set; }
+        public DateTime LastSeen { get; set; }
+        public int ErrorCount { get; set; }
+    }
+}
