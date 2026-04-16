bd809530b09244bb2cb616357940957a532a036a
Timofei Irishiin <eblan2204.1@gmail.com>
2026-04-16 23:23:15 +0300

    Добавление тестов для слоя Models (IPStatistics)
    - Создан класс ModelsTests.cs.
    - Написаны параметризованные тесты [Theory] для тестирования вычисляемого свойства IsSuspicious в классе IPStatistics.
    - Покрыты граничные случаи: значения меньше порога, равные порогу и превышающие порог.

---
+++ Tests/ModelsTests.cs
@@ -0,0 +1,30 @@
+﻿using Models;
+using Xunit;
+
+namespace Tests
+{
+    public class ModelsTests
+    {
+        [Theory]
+        [InlineData(100, 100, false)] // Запросов равно порогу -> не подозрительный
+        [InlineData(99, 100, false)]  // Запросов меньше порога -> не подозрительный
+        [InlineData(101, 100, true)]  // Запросов больше порога -> подозрительный
+        [InlineData(500, 50, true)]   // Запросов сильно больше порога
+        public void IPStatistics_IsSuspicious_ShouldReturnCorrectValue(int requestCount, int threshold, bool expectedIsSuspicious)
+        {
+            // Arrange
+            var ipStats = new IPStatistics
+            {
+                IP = "192.168.1.1",
+                RequestCount = requestCount,
+                SuspiciousThreshold = threshold
+            };
+
+            // Act
+            bool result = ipStats.IsSuspicious;
+
+            // Assert
+            Assert.Equal(expectedIsSuspicious, result);
+        }
+    }
+}
